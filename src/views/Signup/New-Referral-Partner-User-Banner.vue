27444307ea3323d7dcc377008f9b2ca526e51cfb
Barbara Liau <bj.liau@gmail.com>
2017-05-25 13:18:12 -0600

    Add new referral partner banner

---
+++ src/views/Signup/New-Referral-Partner-User-Banner.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div
+    class="alert alert-info text-center"
+    role="alert"
+    data-alert-cookie="alert_new_partner_referraluser"
+  >
+    <span>
+      <p><strong>
+        You've been referred by {{ referralPartner | capitalize }}!
+      </strong></p>
+      <p>
+        Sign up now to unlock <b>one year of free
+        service</b> — up to 25GB of storage and bandwidth
+        every month!
+      </p>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'new-referral-partner-user-banner',
+
+  props: {
+    referralPartner: {
+      type: String
+    }
+  }
+};
+</script>
